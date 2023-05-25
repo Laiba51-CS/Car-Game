91aafc5ba3407557f882b91693b3444c6f7596ba
Laiba <115210630+Laiba51-CS@users.noreply.github.com>
2023-05-25 17:10:00 +0500

    Add files via upload

---
+++ CarGame/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace CarGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.GameLoop = new System.Windows.Forms.Timer(this.components);
+            this.carHealth = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Score1 = new System.Windows.Forms.Label();
+            this.lblscore = new System.Windows.Forms.Label();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pbplayer = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbplayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GameLoop
+            // 
+            this.GameLoop.Enabled = true;
+            this.GameLoop.Tick += new System.EventHandler(this.GameLoop_Tick);
+            // 
+            // carHealth
+            // 
+            this.carHealth.ForeColor = System.Drawing.Color.HotPink;
+            this.carHealth.Location = new System.Drawing.Point(12, 616);
+            this.carHealth.Name = "carHealth";
+            this.carHealth.Size = new System.Drawing.Size(167, 32);
+            this.carHealth.TabIndex = 6;
+            this.carHealth.Value = 100;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 593);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Car Energy";
+            // 
+            // Score1
+            // 
+            this.Score1.AutoSize = true;
+            this.Score1.Font = new System.Drawing.Font("Cooper Black", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Score1.Location = new System.Drawing.Point(6, 235);
+            this.Score1.Name = "Score1";
+            this.Score1.Size = new System.Drawing.Size(95, 32);
+            this.Score1.TabIndex = 8;
+            this.Score1.Text = "Score";
+            this.Score1.Click += new System.EventHandler(this.Score1_Click);
+            // 
+            // lblscore
+            // 
+            this.lblscore.AutoSize = true;
+            this.lblscore.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblscore.Location = new System.Drawing.Point(107, 243);
+            this.lblscore.Name = "lblscore";
+            this.lblscore.Size = new System.Drawing.Size(20, 24);
+            this.lblscore.TabIndex = 9;
+            this.lblscore.Text = "0";
+            this.lblscore.Click += new System.EventHandler(this.Score_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::CarGame.Properties.Resources.Bush;
+            this.pictureBox6.Location = new System.Drawing.Point(737, 418);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(136, 127);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 14;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::CarGame.Properties.Resources.Bush;
+            this.pictureBox4.Location = new System.Drawing.Point(737, 551);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(136, 114);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::CarGame.Properties.Resources.Bush;
+            this.pictureBox3.Location = new System.Drawing.Point(35, 81);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(136, 113);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::CarGame.Properties.Resources.Rock;
+            this.pictureBox2.Location = new System.Drawing.Point(766, 208);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 80);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CarGame.Properties.Resources.Rock;
+            this.pictureBox1.Location = new System.Drawing.Point(71, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pbplayer
+            // 
+            this.pbplayer.BackColor = System.Drawing.Color.Transparent;
+            this.pbplayer.Image = global::CarGame.Properties.Resources.Car_1_01;
+            this.pbplayer.Location = new System.Drawing.Point(414, 537);
+            this.pbplayer.Name = "pbplayer";
+            this.pbplayer.Size = new System.Drawing.Size(100, 128);
+            this.pbplayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbplayer.TabIndex = 1;
+            this.pbplayer.TabStop = false;
+            this.pbplayer.Click += new System.EventHandler(this.pbplayer_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::CarGame.Properties.Resources.Tree1;
+            this.pictureBox5.Location = new System.Drawing.Point(766, 38);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(82, 96);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 15;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.BackgroundImage = global::CarGame.Properties.Resources.road;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(916, 797);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblscore);
+            this.Controls.Add(this.Score1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.carHealth);
+            this.Controls.Add(this.pbplayer);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbplayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pbplayer;
+        private System.Windows.Forms.Timer GameLoop;
+        private System.Windows.Forms.ProgressBar carHealth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Score1;
+        private System.Windows.Forms.Label lblscore;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ CarGame/gameover.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace CarGame
+{
+    partial class gameover
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cancel = new System.Windows.Forms.PictureBox();
+            this.restart = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.cancel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.restart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cancel
+            // 
+            this.cancel.Image = global::CarGame.Properties.Resources.Close_BTN;
+            this.cancel.Location = new System.Drawing.Point(12, 12);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(153, 159);
+            this.cancel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cancel.TabIndex = 0;
+            this.cancel.TabStop = false;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // restart
+            // 
+            this.restart.Image = global::CarGame.Properties.Resources.Play_BTN;
+            this.restart.Location = new System.Drawing.Point(700, 12);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(153, 159);
+            this.restart.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.restart.TabIndex = 1;
+            this.restart.TabStop = false;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // gameover
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CarGame.Properties.Resources.gameover2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(895, 618);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.cancel);
+            this.Name = "gameover";
+            this.Text = "gameover";
+            this.Load += new System.EventHandler(this.gameover_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cancel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.restart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cancel;
+        private System.Windows.Forms.PictureBox restart;
+    }
+}
